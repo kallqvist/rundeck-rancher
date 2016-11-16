d9fa0240b3638cfb71a1e0ca70a91b5e68f0f7a1
klas <klas@vionlabs.com>
2016-11-16 06:57:44 +0100

    improved log timestamp filtering

--- plugins-source/rancher/contents/runonce.py
+++ plugins-source/rancher/contents/runonce.py
@@ -2,24 +2,19 @@ from requests.auth import HTTPBasicAuth
 from dateutil.parser import parse
 import websocket
 import requests
-import datetime
 import logging
 import base64
-import time
 import json
 import os
 import re
-
-# todo: time zone conversion needed?
-script_start_date = datetime.datetime.now()
-
-# todo: how to sync time everywhere? NTP? add delay in execution?
-
 
 
 # todo: abstract away as shared code
 class ErrorLogger(logging.StreamHandler):
     last_error = ''
+    def clear(self):
+        self.last_error = None
+
     def emit(self, record):
         msg = self.format(record)
         self.last_error = msg
@@ -56,6 +51,61 @@ if len(node_id) == 0:
 # if api_res_start.status_code != 200:
 #     raise Exception("Can't start service, code \"{} ({})\"!".format(api_res_start_json['code'], api_res_start_json['status']))
 
+
+log_re_pattern = '^(\d*) (.*?Z) (.*)$'
+
+# setup websocket for reading log output
+api_data_logs = {
+    "follow": True,
+    "lines": 100
+}
+api_url_logs = "{}/containers/{}?action=logs".format(api_base_url, node_id)
+api_res_logs = requests.post(api_url_logs, auth=HTTPBasicAuth(api_access_key, api_secret_key), json=api_data_logs)
+api_res_logs_json = api_res_logs.json()
+# print(api_res_logs_json)
+
+if api_res_logs.status_code != 200:
+    raise Exception("Can't create log listener, code \"{} ({})\"!".format(api_res_logs_json['code'], api_res_logs_json['status']))
+
+ws_url_logs = "{}?token={}".format(api_res_logs_json['url'], api_res_logs_json['token'])
+ws_auth_header = {'Authorization': "Basic {}".format(base64.b64encode("{}:{}".format(api_access_key, api_secret_key)))}
+
+
+
+
+
+#
+# HISTORY LOGS
+#
+
+# read last timestamp from historical logs
+history_logs_last_timestamp = [None]
+def history_logs_on_message(ws, message):
+    msg_match = re.match(log_re_pattern, message)
+    if not msg_match:
+        raise Exception("Failed to read log history, regex does not match!")
+    history_logs_last_timestamp[0] = parse(msg_match.group(2)).replace(tzinfo=None)
+
+history_logs_ws = websocket.WebSocketApp(ws_url_logs,
+    on_message = history_logs_on_message,
+    header = ws_auth_header)
+history_logs_ws.run_forever()
+history_logs_last_timestamp = history_logs_last_timestamp[0]
+
+if history_logs_last_timestamp == None:
+    raise Exception("Failed to read last log timestamp!")
+
+# todo: handle error?
+log_handler.clear()
+
+# todo: start service
+
+
+
+
+#
+# EVENT LISTENER
+#
 def events_on_error(ws, error):
     print("### events stream error ###")
     print error
@@ -72,7 +122,8 @@ def events_on_message(ws, message):
         return
     node_state = json_message["data"]["resource"]["state"]
     print("Container state: {}".format(node_state))
-    if node_state in ["running", "stopping", "stopped"]:
+    # todo: timing issues with short lived containers (already stopped)?
+    if node_state in ["running"]:
         ws.close()
 
 # todo: http?
@@ -84,24 +135,20 @@ ws_events = websocket.WebSocketApp(ws_url_events,
     on_message = events_on_message,
     on_error = events_on_error,
     on_close = events_on_close,
-    header = {'Authorization': "Basic {}".format(base64.b64encode("{}:{}".format(api_access_key, api_secret_key)))})
+    header = ws_auth_header)
 ws_events.run_forever()
 
 # todo: check for errors
+log_handler.clear()
 
-# setup websocket for reading log output
-api_data_logs = {
-    "follow": True,
-    "lines": 100
-}
-api_url_logs = "{}/containers/{}?action=logs".format(api_base_url, node_id)
-api_res_logs = requests.post(api_url_logs, auth=HTTPBasicAuth(api_access_key, api_secret_key), json=api_data_logs)
-api_res_logs_json = api_res_logs.json()
-# print(api_res_logs_json)
 
-if api_res_logs.status_code != 200:
-    raise Exception("Can't create log listener, code \"{} ({})\"!".format(api_res_logs_json['code'], api_res_logs_json['status']))
 
+
+# todo: class?
+
+#
+# LOG LISTENER
+#
 def logs_on_error(ws, error):
     print("### logs stream error ###")
     print(error)
@@ -113,29 +160,27 @@ def logs_on_open(ws):
     print("### logs stream opened ###")
 
 def logs_on_message(ws, message):
-    msg_match = re.match('^(\d*) (.*?Z) (.*)$', message)
+    msg_match = re.match(log_re_pattern, message)
     if not msg_match:
         raise Exception("Failed to read log format, regex does not match!")
 
     is_error = (int(msg_match.group(1)) == 2)
-    # todo: time zone conversion needed?
     log_date = parse(msg_match.group(2)).replace(tzinfo=None)
     log_message = msg_match.group(3)
+    # print("{} - {}".format(log_date, log_message))
 
-    if log_date > script_start_date:
+    if log_date > history_logs_last_timestamp:
         if is_error:
             raise Exception("{} - {}".format(log_date, log_message))
 
-        print("{} - {}".format(log_date, message))
+        print("{} - {}".format(log_date, log_message))
 
-# attach to log listener websocket
-ws_url_logs = "{}?token={}".format(api_res_logs_json['url'], api_res_logs_json['token'])
 ws_logs = websocket.WebSocketApp(ws_url_logs,
     on_open = logs_on_open,
     on_message = logs_on_message,
     on_error = logs_on_error,
     on_close = logs_on_close,
-    header = {'Authorization': "Basic {}".format(base64.b64encode("{}:{}".format(api_access_key, api_secret_key)))})
+    header = ws_auth_header)
 ws_logs.run_forever()
 
 # todo: raise exception if no data is recieved at all?
@@ -144,4 +189,3 @@ if len(log_handler.last_error) > 0:
     raise Exception(log_handler.last_error)
 
 print("=== DONE ===")
-# print(base64.b64decode(ws_res).strip())
