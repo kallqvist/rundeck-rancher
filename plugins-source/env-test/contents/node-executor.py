c1951b6e6b71f99b24e585b4cfd28ae2d3172f34
klas <klas@vionlabs.com>
2016-11-26 12:09:27 +0100

    cleanup

--- plugins-source/env-test/contents/node-executor.py
+++
@@ -1,4 +0,0 @@
-import os
-
-if not 'myvar' in os.environ:
-    raise Exception( 'myvar is not available' )
--- plugins-source/env-test/contents/resource-model.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if not 'myvar' in os.environ:
-    raise Exception( 'myvar is not available' )
-
-print('{ "node1": { "nodename": "node1", "hostname": "node1", "myvar": "' + os.environ['myvar'] + '"} }')
