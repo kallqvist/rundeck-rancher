d6d89a5ce1d1ce4aed420eb4a786dc42b63eb30c
klas <kallqvist@gmail.com>
2016-12-15 14:11:45 +0100

    modified logging output

--- plugins-source/rancher/contents/node-executor.py
+++ plugins-source/rancher/contents/node-executor.py
@@ -1,7 +1,7 @@
 from _nodes_shared import *
 
 pid_check_timeout = 10
-reconnect_timeout = 5
+reconnect_timeout = 1
 
 bash_script = os.environ.get('RD_EXEC_COMMAND', '')
 bash_script = bash_script.strip().encode("string_escape").replace('"', '\\\"')
@@ -10,7 +10,7 @@ bash_script = bash_script.strip().encode("string_escape").replace('"', '\\\"')
 if len(bash_script) == 0:
     raise Exception( "Can't run, command is empty!" )
 
-# check if container is running?
+# todo: check if container is running?
 container_api_url = "{}/container/{}".format(api_base_url, node_id)
 container_api_res = requests.get(container_api_url, auth=api_auth)
 container_api_res_json = container_api_res.json()
@@ -22,7 +22,10 @@ rundeck_exec_id = os.environ.get('RD_JOB_EXECID', '')
 if len(rundeck_project) == 0 or len(rundeck_exec_id) == 0:
     raise Exception("Can't create run ID, RD_RUNDECK_PROJECT or RD_JOB_EXECID is not getting set by rundeck!")
 
-rundeck_job_exec_id = "{}_{}".format(rundeck_project, rundeck_exec_id)
+m = hashlib.md5()
+m.update(bash_script)
+bash_script_md5 = m.hexdigest()
+rundeck_job_exec_id = "{}_{}_{}".format(rundeck_project, rundeck_exec_id, bash_script_md5)
 print("[ I ] Rundeck job execution ID: {}".format(rundeck_job_exec_id))
 
 if container_api_res.status_code != 200:
@@ -57,8 +60,17 @@ if api_res.status_code != 200:
 # Execute command and read output
 #
 ws_url_logs = "{}?token={}".format(api_res_json['url'], api_res_json['token'])
-seen_logs_md5 = []
 
+# we need to open the socket to trigger the command but we wait with reading logs until everything is done
+print("[ I ] Executing command...")
+ws_exec = create_connection(ws_url_logs)
+# result =  ws.recv()
+# print "Received '%s'" % result
+ws_exec.close()
+
+
+
+seen_logs_md5 = []
 def logs_on_message(ws, message):
     message_text = base64.b64decode(message).strip()
 
@@ -91,17 +103,15 @@ def logs_on_message(ws, message):
 
         print(log_message)
 
-# we need to open the socket to trigger the command but we wait with reading logs until it's done
-print("[ I ] Executing command...")
-ws_logs = websocket.WebSocketApp(ws_url_logs,
-    # on_message = logs_on_message,
-    header = ws_auth_header)
-ws_logs.run_forever()
-print("[ I ] Log listener disconnected...")
-
-if log_handler.has_error == True:
-    raise Exception(log_handler.last_error)
-log_handler.clear()
+# ws_logs = websocket.WebSocketApp(ws_url_logs,
+#     # on_message = logs_on_message,
+#     header = ws_auth_header)
+# ws_logs.run_forever()
+# print("[ I ] Log listener disconnected...")
+#
+# if log_handler.has_error == True:
+#     raise Exception(log_handler.last_error)
+# log_handler.clear()
 
 
 
@@ -122,9 +132,28 @@ pid_check_api_url = "{}/containers/{}?action=execute".format(api_base_url, node_
 pid_check_api_res = requests.post(pid_check_api_url, auth=api_auth, json=pid_check_api_data)
 pid_check_api_res_json = pid_check_api_res.json()
 
-print("[ I ] Reconnecting to see if command is done executing and logs are remaining...")
 time.sleep(reconnect_timeout)
 
+# tailing logs while waiting for process to finish
+log_tail_api_data = {
+    "attachStdin": False,
+    "attachStdout": True,
+    "command": [
+      "/bin/sh",
+      "-c",
+      'tail /tmp/{rundeck_job_exec_id}.out'.format(rundeck_job_exec_id=rundeck_job_exec_id)
+    ],
+    "tty": False
+}
+
+# log_tail_api_url = "{}/containers/{}?action=execute".format(api_base_url, node_id)
+# log_tail_api_res = requests.post(log_tail_api_url, auth=api_auth, json=log_tail_api_data)
+# log_tail_api_res_json = log_tail_api_res.json()
+#
+# time.sleep(reconnect_timeout)
+
+
+print("[ I ] Reconnecting to see if command is done executing and logs are remaining...")
 pid_check_result = 1
 while pid_check_result == 1:
     ws_url_pid_check = "{}?token={}".format(pid_check_api_res_json['url'], pid_check_api_res_json['token'])
@@ -135,8 +164,17 @@ while pid_check_result == 1:
         print("[ I ] Process have exited, safe to read logs now...")
         break
     print("[ W ] Process is still running in container, waiting for {} seconds and trying again.".format(pid_check_timeout))
+    time.sleep(pid_check_timeout)
+
+    # todo: tail current log progress? don't know if we can or if rundeck buffer all output until script is done?
+    # ws_url_log_tail = "{}?token={}".format(pid_check_api_res_json['url'], pid_check_api_res_json['token'])
 
 time.sleep(reconnect_timeout)
+
+print("""
+
+Command execution is done, reading complete log output from container storage...
+""")
 
 # Finally, when we're sure the command isn't running anymore we connect one last time to read all logs from disk
 final_log_read_api_data = {
